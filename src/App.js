0fb11448c2041954f54f4030d915c3436ce2469f
Rafa <rafarobles@guelcom.net>
2019-05-07 17:34:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import 'react-w3-components';
-
-
-function App() {
-  return (
-    <div className="App w3-container">
+import React, { Component } from 'react'
+import Faker from 'faker'
+import './App.css'
+ 
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {},
+    }
+  }
+ 
+  componentWillMount() {
+    Faker.locale = "es"
+      const user = {
+        name: Faker.internet.userName(),
+        email: Faker.internet.email(),
+        avatar: Faker.internet.avatar(),
+        company: Faker.company.companyName(),
+        phone: Faker.phone.phoneNumber(),
+        address: Faker.address.streetAddress(),
+        city: Faker.address.city(),
+      }
+      this.setState(() => ({
+        user: user,
+      }))
+    
+  }
+ 
+  renderUsers(user) {
+    return (
+      <div className="w3-container w3-mobile">
         <h2>Card Example</h2>
-        <div className="w3-card-4 w3-display-middle" >
+        <div id="container" className="w3-card-4 w3-display-middle" >
           <header className="w3-container w3-light-grey">
-            <div id="wrapper"></div>
+            <div id="wrapper"> {user.name}<br/></div>
           </header>
           <div className="w3-container">
             <p>New friend request</p>
             <hr/>
-            <div id="avatar" className="w3-left w3-circle " ></div>
-            <div id="info" ></div><br/>
+            <div id="avatar"><img src={user.avatar} className="w3-left w3-circle "/></div>
+            <div id="info" >
+            Email: {user.email}<br/>
+            Company: {user.company}<br/>
+            Phone: {user.phone}<br/>
+            Address: {user.address}<br/>
+            City: {user.city}<br/>
+            </div><br/>
           </div>
           <button className="w3-button w3-block w3-dark-grey">+ Connect</button>
         </div>
-      </div>
-  );
+      </div>   
+    )
+  }
+ 
+  render() {
+    return <div>{this.renderUsers(this.state.user)}</div>
+  }
 }
-
-export default App;
+ 
+export default App
