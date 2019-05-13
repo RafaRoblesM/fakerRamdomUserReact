35d11ed1ab8ae682be4c231be7f28a67f53ff7ec
Rafa <rafarobles@guelcom.net>
2019-05-13 11:39:44 +0200

    render Info since App

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
 import React, { Component } from 'react'
-import Faker from 'faker'
 import Info from './components/info'
 
 import './App.css'
  
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: {},
-    }
-  }
- 
-  componentWillMount() {
-    Faker.locale = "es"
-      const user = {
-        name: Faker.internet.userName(),
-        email: Faker.internet.email(),
-        avatar: Faker.internet.avatar(),
-        company: Faker.company.companyName(),
-        phone: Faker.phone.phoneNumber(),
-        address: Faker.address.streetAddress(),
-        city: Faker.address.city(),
-      }
-      this.setState(() => ({
-        user: user,
-      }))
-  }
-  renderUsers(user) {
-    <Info/>
-  }
-  render() {
-    return <div>{this.renderUsers(this.state.user)}</div>
-  }
+ render(){return(<Info/>)}
 }
  
 export default App
--- src/components/info.js
+++ src/components/info.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react'
+import Faker from 'faker'
 
 class Info extends Component{
-   render(){ 
-       return(
-    <div className="w3-container w3-mobile">
-          <h2>Card Example</h2>
-          <div id="container" className="w3-card-4 w3-display-middle" >
-            <header className="w3-container w3-light-grey">
-              <div id="wrapper"> {user.name}<br/></div>
-            </header>
-            <div className="w3-container">
-              <p>New friend request</p>
-              <hr/>
-              <div id="avatar"><img src={user.avatar} className="w3-left w3-circle "/></div>
-              <div id="info" >
-              Email: {user.email}<br/>
-              Company: {user.company}<br/>
-              Phone: {user.phone}<br/>
-              Address: {user.address}<br/>
-              City: {user.city}<br/>
-              </div><br/>
-            </div>
-            <button className="w3-button w3-block w3-dark-grey">+ Connect</button>
+    constructor(props) {
+        super(props)
+        this.state = {
+          user: {},
+        }
+      }
+     
+      componentWillMount() {
+        Faker.locale = "es"
+          const user = {
+            name: Faker.internet.userName(),
+            email: Faker.internet.email(),
+            avatar: Faker.internet.avatar(),
+            company: Faker.company.companyName(),
+            phone: Faker.phone.phoneNumber(),
+            address: Faker.address.streetAddress(),
+            city: Faker.address.city(),
+          }
+          this.setState(() => ({
+            user: user,
+          }))
+      }
+      renderUsers(user) {
+        return(  <div className="w3-container w3-mobile">
+        <h2>Card Example</h2>
+        <div id="container" className="w3-card-4 w3-display-middle" >
+          <header className="w3-container w3-light-grey">
+            <div id="wrapper"> {user.name}<br/></div>
+          </header>
+          <div className="w3-container">
+            <p>New friend request</p>
+            <hr/>
+            <div id="avatar"><img src={user.avatar} className="w3-left w3-circle "/></div>
+            <div id="info" >
+            Email: {user.email}<br/>
+            Company: {user.company}<br/>
+            Phone: {user.phone}<br/>
+            Address: {user.address}<br/>
+            City: {user.city}<br/>
+            </div><br/>
           </div>
-        </div>   
-  )}
+          <button className="w3-button w3-block w3-dark-grey">+ Connect</button>
+        </div>
+      </div> )
+      }
+      render() {
+        return <div>{this.renderUsers(this.state.user)}</div>
+      }
 }
 
 
